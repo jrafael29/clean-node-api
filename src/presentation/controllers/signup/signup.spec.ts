86790fa7e615cad4372a6198cc760dbd063e2fbe
José Rafael <rafaelsilva9240@hotmail.com>
2024-02-07 00:22:19 -0300

    refactor: move SignUpController from a test file

--- src/presentation/controllers/signup/signup.spec.ts
+++ src/presentation/controllers/signup/signup.spec.ts
@@ -1,50 +1,12 @@
 import {AccountModel, AddAccount, AddAccountModel, Controller, EmailValidator, HttpRequest, HttpResponse} from './signup-protocols'
 import {MissingParamError, ServerError, InvalidParamError} from '../../errors'
-import {badRequest, ok, serverError} from '../../helpers/http-helper'
-
-class SignUpController implements Controller{
-    private readonly emailValidator: EmailValidator
-    private readonly addAccount: AddAccount
-
-    constructor(emailValidator: EmailValidator, addAccount: AddAccount){
-        this.emailValidator = emailValidator
-        this.addAccount = addAccount
-    }
-    async perform (httpRequest: HttpRequest): Promise<HttpResponse> {
-        try{
-            const requiredFields = ['name', 'email', 'password', 'passwordConfirmation'];
-            for(const field of requiredFields){
-                if(!httpRequest.body[field]){
-                    return badRequest(new MissingParamError(field))
-                }
-            }
-            const {name, email, password, passwordConfirmation} = httpRequest.body;
-            if(password !== passwordConfirmation){
-                return badRequest(new InvalidParamError('passwordConfirmation'));
-            }
-            const emailIsValid = this.emailValidator.isValid(email)
-            if(!emailIsValid){
-                return badRequest(new InvalidParamError('email'))
-            }
-            const account = await this.addAccount.add({
-                name,
-                email,
-                password
-            })
-            return ok(account)
-        }catch(error){
-            return serverError();
-        }
-        
-    }
-}
+import { SignUpController } from './SignUpController'
 
 interface SutTypes {
     sut: SignUpController
     emailValidatorStub: EmailValidator
     addAccountStub: AddAccount
 }
-
 
 const makeEmailValidator = (): EmailValidator => {
     class EmailValidatorStub implements EmailValidator {
@@ -54,6 +16,7 @@ const makeEmailValidator = (): EmailValidator => {
     }
     return new EmailValidatorStub();
 }
+
 const makeAddAccount = (): AddAccount => {
     class AddAccountStub implements AddAccount {
         async add(account: AddAccountModel): Promise<AccountModel> {
