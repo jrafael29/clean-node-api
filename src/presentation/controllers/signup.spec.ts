0d98b2e117317b05dec2c475e2c2751c7d7f7403
José Rafael <rafaelsilva9240@hotmail.com>
2024-02-06 22:35:05 -0300

    test: ensure SignUpController returns 400 if no password is provided

--- src/presentation/controllers/signup.spec.ts
+++ src/presentation/controllers/signup.spec.ts
@@ -4,7 +4,7 @@ import {badRequest} from '../helpers/http-helper'
 
 class SignUpController{
     perform (httpRequest: HttpRequest): HttpResponse {
-        const requiredFields = ['name', 'email'];
+        const requiredFields = ['name', 'email', 'password'];
         for(const field of requiredFields){
             if(!httpRequest.body[field]){
                 return badRequest(new MissingParamError(field))
@@ -30,7 +30,7 @@ describe('SignUp Controller', () => {
 
     })
 
-    test('should return 400 if no name is provided', () => {
+    test('should return 400 if no email is provided', () => {
         const httpRequest = {
             body: {
                 name: "any_name",
@@ -43,6 +43,22 @@ describe('SignUp Controller', () => {
 
         expect(httpResponse.statusCode).toBe(400)
         expect(httpResponse.body).toEqual(new MissingParamError('email'))
+
+    })
+
+    test('should return 400 if no password is provided', () => {
+        const httpRequest = {
+            body: {
+                name: "any_name",
+                email: "any_pass",
+                passwordConfirmation: "any_pass"
+            }
+        }
+        const sut = new SignUpController()
+        const httpResponse = sut.perform(httpRequest)
+
+        expect(httpResponse.statusCode).toBe(400)
+        expect(httpResponse.body).toEqual(new MissingParamError('password'))
 
     })
 })
