44be7bed2074789e985dd8464166ace5271164c9
José Rafael <rafaelsilva9240@hotmail.com>
2024-02-10 20:31:17 -0300

    test: ensure BcryptAdapter throw if bcrypt throws

--- src/infra/criptography/bcrypt-adapter.spec.ts
+++ src/infra/criptography/bcrypt-adapter.spec.ts
@@ -1,5 +1,7 @@
 import bcrypt from 'bcrypt'
 import {Encrypter} from '../../data/protocols/encrypter'
+
+
 class BcryptAdapter implements Encrypter{
 
     constructor(private readonly salt: number){}
@@ -10,13 +12,13 @@ class BcryptAdapter implements Encrypter{
     }
 }
 
-jest.mock('bcrypt', () => {
-    return {
-        async hash(): Promise<string> {
-            return new Promise(resolve => resolve("hash"))
-        }
-    }
-})
+// jest.mock('bcrypt', () => {
+//     return {
+//         async hash(): Promise<string> {
+//             return new Promise(resolve => resolve("hash"))
+//         }
+//     }
+// })
 
 type SutTypes = {
     sut: BcryptAdapter
@@ -42,9 +44,23 @@ describe('Bcrypt Adapter', () => {
 
     test('should returns a hash on success', async () => {
         const {sut} = makeSut()
-
+        jest.spyOn(bcrypt, 'hash').mockImplementationOnce(() => {
+            return new Promise(resolve => resolve('hash'))
+        })
         const hash = await sut.encrypt('any_value');
 
         expect(hash).toBe('hash')
+    })
+
+    test('should throw if bcrypt throws', async () => {
+        const {sut} = makeSut()
+
+        jest.spyOn(bcrypt, 'hash').mockImplementationOnce(() => {
+            throw new Error()
+        })
+
+        const promise = sut.encrypt('any_value');
+
+        await expect(promise).rejects.toThrow()
     })
 })
